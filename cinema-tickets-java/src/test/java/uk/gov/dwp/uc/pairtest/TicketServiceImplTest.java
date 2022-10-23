060b74538437c9b7f6dbbc217f03d3688b2b3b2e
Simon Hindle <simon.hindle@engineering.digital.dwp.gov.uk>
2022-10-23 17:13:18 +0100

    Initial solution

---
+++ cinema-tickets-java/src/test/java/uk/gov/dwp/uc/pairtest/TicketServiceImplTest.java
@@ -0,0 +1,144 @@
+package uk.gov.dwp.uc.pairtest;
+
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import thirdparty.paymentgateway.TicketPaymentService;
+import thirdparty.seatbooking.SeatReservationService;
+import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
+import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest.Type;
+import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TicketServiceImplTest {
+
+  @Mock
+  private TicketPaymentService ticketPaymentService;
+  @Mock
+  private SeatReservationService seatReservationService;
+  @InjectMocks
+  private TicketServiceImpl ticketService;
+
+  private static final long ACCOUNT_ID = 123456L;
+
+  @Before
+  public void setUp() {
+    ticketService = new TicketServiceImpl(
+        this.ticketPaymentService,
+        this.seatReservationService
+    );
+  }
+
+  @Test
+  public void accountId_not_present_should_throw_exception() {
+
+    TicketTypeRequest ticketRequest = new TicketTypeRequest(Type.ADULT, 1);
+
+    InvalidPurchaseException thrown =
+        assertThrows(InvalidPurchaseException.class,
+            () ->
+                ticketService.purchaseTickets(null, ticketRequest));
+
+    assertTrue(thrown.getMessage().contains("Account ID must be present and greater than zero"));
+  }
+
+  @Test
+  public void accountId_is_zero_should_throw_exception() {
+
+    TicketTypeRequest ticketRequest = new TicketTypeRequest(Type.ADULT, 1);
+
+    InvalidPurchaseException thrown =
+        assertThrows(InvalidPurchaseException.class,
+            () ->
+                ticketService.purchaseTickets(0L, ticketRequest));
+
+    assertTrue(thrown.getMessage().contains("Account ID must be present and greater than zero"));
+  }
+
+  @Test
+  public void accountId_less_than_zero_should_throw_exception() {
+
+    TicketTypeRequest ticketRequest = new TicketTypeRequest(Type.ADULT, 1);
+
+    InvalidPurchaseException thrown =
+        assertThrows(InvalidPurchaseException.class,
+            () ->
+                ticketService.purchaseTickets(-1L, ticketRequest));
+
+    assertTrue(thrown.getMessage().contains("Account ID must be present and greater than zero"));
+  }
+
+  @Test
+  public void valid_ticket_request_should_request_payment_and_reserve_seats() {
+
+    TicketTypeRequest adultTicketRequest = new TicketTypeRequest(Type.ADULT, 4);
+    TicketTypeRequest childTicketRequest = new TicketTypeRequest(Type.CHILD, 2);
+    TicketTypeRequest infantTicketRequest = new TicketTypeRequest(Type.INFANT, 2);
+
+    ticketService.purchaseTickets(ACCOUNT_ID, adultTicketRequest, childTicketRequest, infantTicketRequest );
+
+    verify(ticketPaymentService, times(1)).makePayment(ACCOUNT_ID, 100);
+    verify(seatReservationService, times(1)).reserveSeat(ACCOUNT_ID, 6);
+  }
+
+  @Test
+  public void total_tickets_ordered_over_20_should_throw_exception() {
+
+    TicketTypeRequest adultTicketRequest = new TicketTypeRequest(Type.ADULT, 19);
+    TicketTypeRequest childTicketRequest = new TicketTypeRequest(Type.CHILD, 2);
+
+    InvalidPurchaseException thrown =
+        assertThrows(InvalidPurchaseException.class,
+            () -> ticketService.purchaseTickets(ACCOUNT_ID, adultTicketRequest, childTicketRequest));
+
+    assertTrue(thrown.getMessage().contains("Total tickets ordered exceeds maximum allowed: 20"));
+  }
+
+  @Test
+  public void child_tickets_ordered_no_adult_tickets_should_throw_exception() {
+
+    TicketTypeRequest childTicketRequest = new TicketTypeRequest(Type.CHILD, 2);
+
+    InvalidPurchaseException thrown =
+        assertThrows(InvalidPurchaseException.class,
+            () -> ticketService.purchaseTickets(ACCOUNT_ID, childTicketRequest));
+
+    assertTrue(thrown.getMessage()
+        .contains("Cannot purchase child or infant tickets without also purchasing an adult ticket"));
+  }
+
+  @Test
+  public void infant_tickets_ordered_no_adult_tickets_should_throw_exception() {
+
+    TicketTypeRequest infantTicketRequest = new TicketTypeRequest(Type.INFANT, 2);
+
+    InvalidPurchaseException thrown =
+        assertThrows(InvalidPurchaseException.class,
+            () -> ticketService.purchaseTickets(ACCOUNT_ID, infantTicketRequest));
+
+    assertTrue(thrown.getMessage()
+        .contains("Cannot purchase child or infant tickets without also purchasing an adult ticket"));
+  }
+
+  @Test
+  public void child_and_infant_tickets_ordered_no_adult_tickets_should_throw_exception() {
+
+    TicketTypeRequest childTicketRequest = new TicketTypeRequest(Type.CHILD, 2);
+    TicketTypeRequest infantTicketRequest = new TicketTypeRequest(Type.INFANT, 1);
+
+    InvalidPurchaseException thrown =
+        assertThrows(InvalidPurchaseException.class,
+            () -> ticketService.purchaseTickets(ACCOUNT_ID, childTicketRequest, infantTicketRequest));
+
+    assertTrue(thrown.getMessage()
+        .contains("Cannot purchase child or infant tickets without also purchasing an adult ticket"));
+  }
+}
