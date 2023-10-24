1dad2812cccb3cf23712f253acde5019b7207bff
simon.hindle <simon.hindle@engineering.digital.dwp.gov.uk>
2023-10-24 21:55:55 +0100

    Solution

---
+++ src/main/java/uk/gov/dwp/uc/pairtest/domain/TicketTypeRequest.java
@@ -0,0 +1,26 @@
+package uk.gov.dwp.uc.pairtest.domain;
+
+public class TicketTypeRequest {
+
+  private final int noOfTickets;
+  private final Type type;
+
+  public TicketTypeRequest(Type type, int noOfTickets) {
+    this.type = type;
+    this.noOfTickets = noOfTickets;
+  }
+
+  public int getNoOfTickets() {
+    return noOfTickets;
+  }
+
+  public Type getTicketType() {
+    return type;
+  }
+
+  public enum Type {
+    ADULT,
+    CHILD,
+    INFANT
+  }
+}
